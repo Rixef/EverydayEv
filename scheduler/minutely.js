50a201e20edb851c2e9d883ba96da3a8f677613c
Rixef <74892049+Rixef@users.noreply.github.com>
2022-04-22 02:11:32 -0700

    Update minutely.js
    Fix math errors
    remainingdays
    remaininghours
    remainingminutes

--- scheduler/minutely.js
+++ scheduler/minutely.js
@@ -1,9 +1,11 @@
 const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
 
 var TZs = {
+  "EDT": -04*60,
   "ET": -05*60,
   "CT": -06*60,
   "MT": -07*60,
+  "PDT": -07*60,
   "PT": -08*60,
   "AK": -09*60,
   "HAST": -10*60,
@@ -179,7 +181,7 @@ if(messageContents.length > 0)//if it does exist already
   var diffdays = diffhours/24;
   var remainingdays = Math.floor(diffdays);
   var remaininghours = Math.floor(diffhours % 24);
-  var remainingminutes = Math.floor(getsetminute - nowminute);
+  var remainingminutes = Math.floor(diffmins % 60);
   if(remainingdays < 0 || remaininghours < 0 || remainingminutes < 0)
   {
     await lib.discord.channels['@0.3.0'].messages.destroy({
